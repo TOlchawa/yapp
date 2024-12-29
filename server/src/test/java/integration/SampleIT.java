350e88f8fa234ab8756cbdb95fba8a19b64740a9
tolchawa <tolchawa@gmail.com>
2024-12-29 20:39:48 +0100

    setup integration tests

--- server/src/test/java/integration/SampleIT.java
+++ server/src/test/java/integration/SampleIT.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @Slf4j
 @Tag("integration")
 public class SampleIT {
 
     @Test
     void testIntegrationScenario() {
-        log.info("Integration test scenario");
+        String test = "ok";
+        assertThat(test).isEqualTo("ok");
     }
 
 }
