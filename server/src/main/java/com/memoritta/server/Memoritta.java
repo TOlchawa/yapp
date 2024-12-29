29f27e8640819850a01fef94d9cc98c6b7cf29f1
tolchawa <tolchawa@gmail.com>
2024-12-29 19:10:19 +0100

    fix pom.xml

---
+++ server/src/main/java/com/memoritta/server/Memoritta.java
@@ -0,0 +1,11 @@
+package com.memoritta.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Memoritta {
+    public static void main(String[] args) {
+        SpringApplication.run(Memoritta.class, args);
+    }
+}
