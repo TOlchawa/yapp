a35b7cc2626d0ac37b4f0bf487e4b9654817b4d3
tolchawa <tolchawa@gmail.com>
2024-12-29 20:55:51 +0100

    setup integration tests wth rest assured

--- server/src/test/java/integration/SampleIT.java
+++ server/src/test/java/integration/SampleIT.java
@@ -1,10 +1,13 @@
 package integration;
 
+import io.restassured.RestAssured;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
@@ -13,8 +16,16 @@ public class SampleIT {
 
     @Test
     void testIntegrationScenario() {
-        String test = "ok";
-        assertThat(test).isEqualTo("ok");
+
+        RestAssured.baseURI = "http://localhost:9090";
+
+        given()
+                .when()
+                .get("/version")
+                .then()
+                .statusCode(200)
+                .body(equalTo("1.0.0"));
+
     }
 
 }
