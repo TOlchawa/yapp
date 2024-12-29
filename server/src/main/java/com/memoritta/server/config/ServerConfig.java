8acd16c0a06e0fcdb58fc0acbe7d633928b53f2b
tolchawa <tolchawa@gmail.com>
2024-12-29 19:29:49 +0100

    enable sample endpoint

--- server/src/main/java/com/memoritta/server/config/ServerConfig.java
+++ server/src/main/java/com/memoritta/server/config/ServerConfig.java
@@ -2,7 +2,9 @@ package com.memoritta.server.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @Setter
 @Getter
 public class ServerConfig {
