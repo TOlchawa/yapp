3b5fae8081d96d94a51558a73599d373b073ddc8
tolchawa <tolchawa@gmail.com>
2025-03-19 18:08:33 +0100

    #29 : Setup DB for tests (H2 or something) - setup e2e for github admin/admin

--- server/src/test/java/integration/SampleIT.java
+++ server/src/test/java/integration/SampleIT.java
@@ -17,7 +17,7 @@ public class SampleIT {
     @Test
     void testIntegrationScenario() {
 
-        RestAssured.baseURI = "http://127.0.0.1:9090";
+        RestAssured.baseURI = "http://localhost:9090";
 
         given()
                 .when()
