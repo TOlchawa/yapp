546210d3a9903781f65828bf434355f7e005eb96
Tomasz Olchawa <13537905+TOlchawa@users.noreply.github.com>
2025-06-08 12:30:15 +0200

    Add Friends view and basic test

---
+++ frontend/src/Friends.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Friends from './Friends.jsx';
+
+describe('Friends component', () => {
+  it('shows title', () => {
+    render(<Friends />);
+    expect(screen.getByRole('heading', { level: 1, name: 'Friends' })).toBeInTheDocument();
+  });
+});
