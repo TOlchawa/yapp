43ae93bfc14abed4fe1d7ec49e2da798291be999
Tomasz Olchawa <13537905+TOlchawa@users.noreply.github.com>
2025-07-06 21:38:17 +0200

    Replace printStackTrace with logger in OpenApiExceptionLogger (#236)

--- server/src/main/java/com/memoritta/server/advice/OpenApiExceptionLogger.java
+++ server/src/main/java/com/memoritta/server/advice/OpenApiExceptionLogger.java
@@ -4,12 +4,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class OpenApiExceptionLogger extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -19,7 +21,7 @@ public class OpenApiExceptionLogger extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         } catch (Throwable t) {
             if (request.getRequestURI().contains("/v3/api-docs")) {
-                t.printStackTrace();
+                log.warn("Error while processing /v3/api-docs: {}", t.getMessage());
             }
             throw t;
         }
